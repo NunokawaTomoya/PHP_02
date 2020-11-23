74a7f2f9711ebf497ddb7efc49658dd5809d1ec7
Nunokawa <nunokawa219@i.softbank.jp>
2020-11-23 15:59:04 +0000

    1

---
+++ practice.php
@@ -0,0 +1,64 @@
+<?php
+//課題1
+$a = 3;
+$b = 7;
+
+echo $a + $b;
+echo "\n";
+
+
+//課題2
+$array_month = [
+    "1"  => "1月",
+    "2"  => "2月",
+    "3"  => "3月",
+    "4"  => "4月",
+    "5"  => "5月",
+    "6"  => "6月",
+    "7"  => "7月",
+    "8"  => "8月",
+    "9"  => "9月",
+    "10" => "10月",
+    "11" => "11月",
+    "12" => "12月"
+];
+
+echo $array_month[8];
+echo "\n";
+
+
+//課題3
+$hello = "Hello, ";
+$name  = "Nunokawa";
+$world = "'s World!";
+
+echo $hello .$name .$world;
+echo "\n";
+
+
+//課題4
+$tech_boost = "tech ";
+$tech_boost .= "boost";
+
+echo $tech_boost;
+echo "\n";
+
+
+//課題5
+$calendar_2018 = [
+    "January"   => "1月",
+    "February"  => "2月",
+    "March"     => "3月",
+    "April"     => "4月",
+    "May"       => "5月",
+    "June"      => "6月",
+    "July"      => "7月",
+    "August"    => "8月",
+    "September" => "9月",
+    "October"   => "10月",
+    "November"  => "11月",
+    "December"  => "12月"
+];
+
+echo $calendar_2018['December'];
+echo "\n";
